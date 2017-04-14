c02eee9adffaf0d5542045561db37ebc484aa4ab
Otoser <amelkin-98@mail.ru>
2017-04-14 17:10:35 +0700

    Add files via upload

---
+++ Basket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domens
+{
+    public class Basket
+    {
+        public string DeliveryMethod { get; set; }
+        public string DeliveryTime { get; set; }
+        public string WhereToDeliver { get; set; }
+        public string TotalPrice { get; set; }
+    }
+}
